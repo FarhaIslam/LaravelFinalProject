4925bdbd0adfc6b3a66839f628e40d0a5e4c0c5a
FarhaIslam <30765314+FarhaIslam@users.noreply.github.com>
2020-01-09 07:31:03 -0800

    Add files via upload

---
+++ database/migrations/2020_01_07_170051_create_delivery_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery', function (Blueprint $table) {
+            $table->increments('delivery_id');
+            $table->string('delivery_name');
+            $table->string('delivery_address');
+            $table->string('delivery_telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery');
+    }
+}
